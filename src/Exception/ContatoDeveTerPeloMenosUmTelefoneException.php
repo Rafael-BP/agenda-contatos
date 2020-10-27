8f4fb88f4d3b0899197070c138a3ca055af0c8d8
Rafael-BP <rafael_bartalotti@hotmail.com>
2020-10-27 02:29:29 -0300

    feat: API básica de Agenda de Contatos em SF4

---
+++ src/Exception/ContatoDeveTerPeloMenosUmTelefoneException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exception;
+
+/**
+ * ContatoDeveTerPeloMenosUmTelefone
+ */
+class ContatoDeveTerPeloMenosUmTelefoneException extends \DomainException
+{
+}
---
+++ src/Exception/TelefoneInvalidoException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exception;
+
+/**
+ * TelefoneInvalido
+ */
+class TelefoneInvalidoException extends \InvalidArgumentException
+{
+}
